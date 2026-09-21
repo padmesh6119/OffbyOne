99ca7115da806e452b56bdb5877e0422381f8866
padmesh6119 <padmesh77123@gmail.com>
2026-09-22 02:33:20 +0530

    exclude auto UserDetailsService to fix 403 on auth endpoints

--- backend/src/main/java/com/offbyone/OffByOneApplication.java
+++ backend/src/main/java/com/offbyone/OffByOneApplication.java
@@ -2,8 +2,9 @@ package com.offbyone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class OffByOneApplication {
     public static void main(String[] args) {
         SpringApplication.run(OffByOneApplication.class, args);
